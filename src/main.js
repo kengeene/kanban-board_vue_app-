3819f38a73577f22ea0fdef39e3e2e89329bdc67
enkuria <eugene@sendyit.com>
2023-06-12 22:46:51 +0300

    build: change vue to version 3

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
-import Vue from 'vue';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'element-plus/dist/index.css';
 
-Vue.config.productionTip = false;
-
-Vue.use(ElementPlus);
-
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+const app = createApp(App);
+app.use(router).use(ElementPlus).use(store).mount('#app');
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import { createStore } from 'vuex';
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
-  state: {
+// Create a new store instance.
+const store = createStore({
+  state() {
+    return {
+    };
   },
   mutations: {
   },
   actions: {
+
   },
-  modules: {
+  getters: {
+
   },
 });
+
+export default store;
