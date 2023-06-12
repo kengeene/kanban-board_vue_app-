fed2f5b4a86baa630fe549234e1b165726b832ff
enkuria <eugene@sendyit.com>
2023-06-12 21:01:16 +0300

    refactor: add lint changes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import ElementPlus from 'element-plus';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'element-plus/dist/index.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(ElementPlus);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
