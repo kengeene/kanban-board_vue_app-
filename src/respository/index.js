f45546e079d938cf463c6676875ab13248df9489
enkuria <eugene@sendyit.com>
2023-06-13 11:28:14 +0300

    feat: add faker and create repository

---
+++ src/respository/index.js
@@ -0,0 +1,34 @@
+const { faker } = require('@faker-js/faker');
+
+module.exports = () => {
+  const data = {
+    tasks: [],
+    users: [],
+    taskStatuses: ['Pending', 'In Progress', 'In QA', 'Done'],
+    taskTypes: ['Bug', 'Feature'],
+  };
+
+  for (let i = 0; i < 10; i++) {
+    data.users.push({
+      id: faker.string.uuid(),
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      avatar: faker.internet.avatar(),
+    });
+  }
+
+  for (let i = 0; i < 15; i++) {
+    // To help randomly assign a user to tasks
+    const randomUsersindex = Math.floor(Math.random() * data.users.length);
+
+    data.tasks.push({
+      title: faker.lorem.words(5),
+      taskType: faker.helpers.arrayElement(data.taskTypes),
+      taskId: faker.string.alphanumeric({ casing: 'upper', length: 3 }) + faker.string.numeric(3),
+      taskStatus: faker.helpers.arrayElement(data.taskStatuses),
+      userId: data.users[randomUsersindex].id,
+      userAvatar: data.users[randomUsersindex].avatar,
+    });
+  }
+  return data;
+};
