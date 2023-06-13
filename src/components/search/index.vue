f10e38784b7131d649f11e4110c3812ad983e158
enkuria <eugene@sendyit.com>
2023-06-13 17:10:03 +0300

    feat: add searchbar component

---
+++ src/components/search/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <el-input placeholder="Search" v-model="search">
+      <template v-slot:prepend >
+        <el-button icon="el-icon-search"></el-button>
+      </template>
+    </el-input>
+    </div>
+</template>
+
+<script>
+import useTasks from '@/composibles/useTasks';
+
+export default {
+  name: 'Search',
+  setup() {
+    const { search } = useTasks();
+
+    return {
+      search,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
+    <search-component/>
     <tasks/>
   </div>
 </template>
 
 <script>
 import tasks from '@/components/tasks.vue';
+import searchComponent from '@/components/search/index.vue';
 
 export default {
   name: 'Home',
   components: {
     tasks,
+    searchComponent,
   },
 };
 </script>
