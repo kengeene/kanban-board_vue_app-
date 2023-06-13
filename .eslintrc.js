d16f1522e1fad602ec9e81c6a870520e62f88183
enkuria <eugene@sendyit.com>
2023-06-13 19:14:30 +0300

    chore: add prettier to format code

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,23 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/airbnb'],
+  extends: ["plugin:vue/recommended", "eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "@babel/eslint-parser",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/multi-word-component-names': 'off',
-    'import/prefer-default-export': 'off',
-    'implicit-arrow-linebreak': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'no-plusplus': 'off',
+    "prettier/prettier": "error",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": "off",
+    "import/prefer-default-export": "off",
+    "implicit-arrow-linebreak": "off",
+    "import/no-extraneous-dependencies": "off",
+    "no-plusplus": "off",
   },
   overrides: [
     {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
       env: {
         jest: true,
       },
