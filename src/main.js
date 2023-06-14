d01dba9625fe57ad823460308f442bc5ad2c0394
enkuria <eugene@sendyit.com>
2023-06-14 10:48:38 +0300

    feat: element ui icons

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
 app.use(router).use(ElementPlus).use(store).mount("#app");
