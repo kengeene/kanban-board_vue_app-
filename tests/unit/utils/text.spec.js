5c8842bb81d45b351ae5142b6654e318396daf45
enkuria <kengeene@gmail.com>
2023-06-17 20:43:47 +0300

    test: add unit test for text utils

---
+++ tests/unit/utils/text.spec.js
@@ -0,0 +1,21 @@
+import { copyToClipboard, generateProjectId } from "@/utils/text.js";
+
+const writeText = jest.fn();
+
+Object.assign(navigator, {
+  clipboard: {
+    writeText,
+  },
+});
+
+describe("text utils", () => {
+  it("Should be able to generate a project ID", () => {
+    const projectId = generateProjectId();
+    expect(projectId).toMatch(/^[[A-Z]|[0-9]]{3}-[[A-Z]|[0-9]]{3}$/g);
+  });
+
+  it("Should be able to copy text to clipboard", () => {
+    copyToClipboard("this is copied");
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith("this is copied");
+  });
+});
