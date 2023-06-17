88605220b82982946744f530f3b077eeb3cfb385
enkuria <kengeene@gmail.com>
2023-06-17 18:44:56 +0300

    feat: add generate projectId

--- src/utils/text.js
+++ src/utils/text.js
@@ -1,5 +1,6 @@
 import useNotifications from "@/utils/notifications";
 
+const { faker } = require("@faker-js/faker");
 const { showNotification } = useNotifications();
 
 export async function copyToClipboard(text) {
@@ -9,4 +10,8 @@ export async function copyToClipboard(text) {
   } catch (e) {
     showNotification("error", "We couldn't copy your link to the clipboard");
   }
+}
+
+export function generateProjectId() {
+  return `${faker.string.alphanumeric({ casing: "upper", length: 3 })}-${faker.string.numeric(3)}`;
 }
