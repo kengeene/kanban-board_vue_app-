de37b9950bb3d8aadbdb72d9e149f375c937fa15
enkuria <kengeene@gmail.com>
2023-06-17 21:54:05 +0300

    fix: failing unit test

--- tests/unit/utils/text.spec.js
+++ tests/unit/utils/text.spec.js
@@ -11,7 +11,7 @@ Object.assign(navigator, {
 describe("text utils", () => {
   it("Should be able to generate a project ID", () => {
     const projectId = generateProjectId();
-    expect(projectId).toMatch(/^[[A-Z]|[0-9]]{3}-[[A-Z]|[0-9]]{3}$/g);
+    expect(projectId).toMatch(/^([A-Z]|[0-9]){3}-([A-Z]|[0-9]){3}$/g);
   });
 
   it("Should be able to copy text to clipboard", () => {
